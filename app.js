b90776d3790646a592d75359f5b936a20f9dc61c
Chanawatn Nakwangsarth <chanawatnpound@gmail.com>
2017-05-03 08:47:42 +0700

    Test with Bizagi --> result : Pass ^^

--- app.js
+++ app.js
@@ -1,12 +1,76 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var mongodb = require('mongodb');
+
+var Chance = require("chance");
+var chance = new Chance();
+
 var app = express();
  
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
  
 var routes = require("./routes/routes.js")(app);
- 
-var server = app.listen(3000, function () {
+
+var MongoClient = require('mongodb').MongoClient;
+var db;
+
+// Initialize connection once
+MongoClient.connect("mongodb://localhost:27017/tmb_rest_apis", function(err, database) {
+  if(err) throw err;
+
+  db = database;
+
+  // Start the application after the database connection is ready
+  var server = app.listen(3000, function () {
+    console.log("Successfully connected to " + database.databaseName);
     console.log("Listening on port %s...", server.address().port);
+    });
+});
+
+// Reuse database object in request handlers
+app.get("/", function(req, res) {
+
+var reqUserName = req.query.username;
+
+var results;
+
+db.collection("mongo_cases").find({"case_at_user" : reqUserName}).toArray(function(err, data) {
+
+        results = {
+            "cases" : data
+        };
+        
+        
+        
+
+        if (err) {
+            res(err);
+            console.log(err);
+        } else {
+            //console.log(data);
+            res.json(results)
+
+
+        }
+    });
+});
+
+app.get('/account', function (req, res) {
+
+    var reqName = req.query.name;
+
+    var gen_pass = chance.string();
+    var accountMock = {
+        accountCollection : [
+            {
+                "username": reqName,
+                "password": gen_pass,
+                "twitter": "@"+reqName
+            }
+        ]
+    }
+
+    return res.send(accountMock);
+
 });
--- routes/routes.js
+++ routes/routes.js
@@ -2,40 +2,6 @@ var Chance = require("chance");
 var chance = new Chance();
 
 var appRouter = function(app) {
-
-app.get("/", function(req, res) {
-    res.send("Hello World");
-});
-
-app.get("/account", function(req, res) {
-
-    var gen_name = chance.name();
-    var gen_pass = chance.string();
-
-    var accountMock = {
-
-        "user_account": {
-            "username": gen_name,
-            "password": gen_pass,
-            "twitter": "@"+gen_name
-        }
-    }
-    // if(!req.query.username) {
-    //     return res.send({"status": "error", "message": "missing username"});
-    // } else if(req.query.username != accountMock.username) {
-    //     return res.send({"status": "error", "message": "wrong username"});
-    // } else {
-        return res.send(accountMock);
-    // }
-});
-
-// app.post("/account", function(req, res) {
-//     if(!req.body.username || !req.body.password || !req.body.twitter) {
-//         return res.send({"status": "error", "message": "missing a parameter"});
-//     } else {
-//         return res.send(req.body);
-//     }
-// });
  
 }
  
