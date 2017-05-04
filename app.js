6413d878dbf30042a9c0eab344aab0a27c089049
Chanawatn Nakwangsarth <chanawatnpound@gmail.com>
2017-05-04 21:48:20 +0700

    use mongoose to connect to db

--- app.js
+++ app.js
@@ -12,21 +12,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
  
 var routes = require("./routes/routes.js")(app);
 
-var MongoClient = require('mongodb').MongoClient;
-var db;
-
-// Initialize connection once
-MongoClient.connect("mongodb://localhost:27017/tmb_rest_apis", function(err, database) {
-  if(err) throw err;
-
-  db = database;
-
-  // Start the application after the database connection is ready
-  var server = app.listen(3000, function () {
-    console.log("Successfully connected to " + database.databaseName);
-    console.log("Listening on port %s...", server.address().port);
+//Connect to mongodb
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/tmb_rest_apis');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+    var server = app.listen(3000, function () {
+        console.log("Successfully connected to " + db.name);
+        console.log("Listening on port %s...", server.address().port);
     });
 });
+
 
 // Reuse database object in request handlers
 app.get("/", function(req, res) {
@@ -35,14 +33,23 @@ var reqUserName = req.query.username;
 
 var results;
 
-db.collection("mongo_cases").find({"case_at_user" : reqUserName}).toArray(function(err, data) {
+db.collection("mongo_cases").find( { $and: [{case_at_user :  reqUserName }, { case_status : { $ne: "Closed" } } ]}).toArray(function(err, data) {
+
+        for (var i in data) {
+            //var rDate = Date(data[i].case_received_date);
+            //date.format("dd/mm/yyyy");
+            //console.log(date);
+            //var rDate_str = rDate.toLocaleDateString();
+            var jsonDate = (new Date(data[i].case_received_date)).toJSON();
+            var backToDate = new Date(jsonDate);
+            //console.log(jsonDate);
+
+            data[i].case_received_date = jsonDate;
+        }
 
         results = {
             "cases" : data
         };
-        
-        
-        
 
         if (err) {
             res(err);
@@ -73,4 +80,36 @@ app.get('/account', function (req, res) {
 
     return res.send(accountMock);
 
+});
+
+app.get('/update', function (req, res) {
+
+    var reqName = req.query.username;
+    console.log(reqName);
+    var reqApp = req.query.app;
+    console.log(reqApp);
+    var results;
+
+    results = { case : 
+        {
+            result : "Success"
+        } 
+    }
+
+    db.collection("mongo_cases").findAndModify(
+    { case_at_user : reqName , case_application_no : reqApp },
+    [], 
+    { $set: { case_status : "afterNCB" } },
+    { upsert: true }, 
+    function (err, doc) {
+
+        if (err) {
+            res(err);
+            console.log(err);
+        } else {
+            //console.log(data);
+            res.json(results)
+        }
+    });
+    
 });
